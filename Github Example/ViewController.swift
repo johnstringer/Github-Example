f9380930fecbd6a80f19912960d4d0cdee5d6b71
John Stringer <stringerjohn@outlook.com>
2021-01-21 10:56:16 +0800

    Play game commit

--- Github Example/ViewController.swift
+++ Github Example/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         print("Do something")
         
     }
+    @IBAction func PlayGame(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
