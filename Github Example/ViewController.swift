afe66139fa41bd417f2e6f73b0e31eef1401e02b
John Stringer <stringerjohn@outlook.com>
2021-01-21 10:52:31 +0800

    Initial commit

--- Github Example/ViewController.swift
+++ Github Example/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func Button(_ sender: UIButton) {
+        
+        print("Do something")
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
